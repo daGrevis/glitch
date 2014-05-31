59c6999ba498cf2484360082863b075964e9f965
Raitis Stengrevics <dagrevis@gmail.com>
2014-05-31 15:47:21 +0300

    Add configuration file and many links

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import logging
-from os.path import exists
+import json
+import os
+from os import path
 
 import requests
 from selenium import webdriver
 from PIL import Image, ImageChops
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -12,8 +13,8 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 logger.addHandler(console_handler)
 
-
-LINK = "http://127.0.0.1:4000/"
+FILENAME_FOR_CONFIGURATION = "Glitchfile"
+PATH_TO_SCREENSHOTS_DIRECTORY = "screenshots/"
 FILENAME_FOR_SCREENSHOT_BEFORE = "screenshot_before.png"
 FILENAME_FOR_SCREENSHOT_AFTER = "screenshot_after.png"
 FILENAME_FOR_SCREENSHOT_DIFFERENCE = "screenshot_difference.png"
@@ -25,32 +26,79 @@ def save_screenshot(link, filename):
     browser.save_screenshot(filename)
     browser.quit()
 
-
 try:
-    requests.get(LINK)
-except requests.exceptions.ConnectionError:
-    logger.error("Could not open the link! Is the server down?")
-
+    with open(FILENAME_FOR_CONFIGURATION) as configuration:
+        configuration_content = configuration.read()
+except IOError:
+    message = ("Could not open `{}`. Is it there?"
+               .format(FILENAME_FOR_CONFIGURATION))
+    logger.error(message)
     exit()
 
-if not exists(FILENAME_FOR_SCREENSHOT_BEFORE):
-    logger.info("No screenshot was found, creating one.")
+try:
+    configuration = json.loads(configuration_content)
+except ValueError:
+    message = ("Could not parse `{}`. Is it valid JSON?"
+               .format(FILENAME_FOR_CONFIGURATION))
+    logger.error(message)
+    exit()
 
-    save_screenshot(LINK, FILENAME_FOR_SCREENSHOT_BEFORE)
-else:
-    logger.info("Screenshot was found, creating a new one for comparison.")
+try:
+    os.mkdir(PATH_TO_SCREENSHOTS_DIRECTORY)
+except OSError:
+    pass
 
-    save_screenshot(LINK, FILENAME_FOR_SCREENSHOT_AFTER)
-    screenshot_before = Image.open(FILENAME_FOR_SCREENSHOT_BEFORE)
-    screenshot_after = Image.open(FILENAME_FOR_SCREENSHOT_AFTER)
+for link in configuration["links"]:
+    try:
+        response = requests.get(link)
+    except requests.exceptions.ConnectionError:
+        logger.error("Could not open `{}`. Is the server down?".format(link))
+        exit()
 
-    if screenshot_before.tostring() == screenshot_after.tostring():
-        logger.info("Screenshots are identical!")
+    if response.status_code != 200:
+        message = ("Could not read `{}`. Is is there?"
+                   .format(link))
+        logger.error(message)
+        exit()
+
+    path_to_screenshots_directory = path.join(PATH_TO_SCREENSHOTS_DIRECTORY,
+                                              link.replace("/", "-"))
+
+    try:
+        os.mkdir(path_to_screenshots_directory)
+    except OSError:
+        pass
+
+    path_to_screenshot_before = path.join(path_to_screenshots_directory,
+                                          FILENAME_FOR_SCREENSHOT_BEFORE)
+
+    if not path.exists(path_to_screenshot_before):
+        save_screenshot(link, path_to_screenshot_before)
+
+        logger.info("No screenshot was found, creating one.")
     else:
-        difference = ImageChops.difference(screenshot_before, screenshot_after)
-        difference = ImageChops.invert(difference)
-        difference.save(FILENAME_FOR_SCREENSHOT_DIFFERENCE)
+        logger.info("Screenshot was found, creating a new one for comparison.")
 
-        message = ("Screenshots differ, see `{}`."
-                   .format(FILENAME_FOR_SCREENSHOT_DIFFERENCE))
-        logger.info(message)
+        path_to_screenshot_after = path.join(path_to_screenshots_directory,
+                                             FILENAME_FOR_SCREENSHOT_AFTER)
+        save_screenshot(link, path_to_screenshot_after)
+
+        screenshot_before = Image.open(path_to_screenshot_before)
+        screenshot_after = Image.open(path_to_screenshot_after)
+
+        if screenshot_before.tostring() == screenshot_after.tostring():
+            logger.info("Screenshots are identical!")
+        else:
+            path_to_screenshot_difference = path.join(
+                path_to_screenshots_directory,
+                FILENAME_FOR_SCREENSHOT_DIFFERENCE)
+
+            difference = ImageChops.difference(screenshot_before,
+                                               screenshot_after)
+            difference = ImageChops.invert(difference)
+            difference.save(path_to_screenshot_difference)
+
+            message = ("Screenshots differ, see `{}`."
+                       .format(path_to_screenshot_difference))
+            logger.info(message)
+            exit()
